111c06647dcc3a0865f7cc2bdda61f8bbf53a507
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-03-24 23:03:29 +0530

    Updating

--- A_new.py
+++ A_new.py
@@ -1,9 +1,16 @@
-def linear_search(arr, target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right)//2
+        if arr[mid]== target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
     return -1
 
-arr = [10,20,30,40,50,60]
-target = 40
-print(linear_search(arr, target))
+arr = [29,293,48,504,492,889,37662,948472,28283634546]
+target = 889
+print(binary_search(arr, target))
