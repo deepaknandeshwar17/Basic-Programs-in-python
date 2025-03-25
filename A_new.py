96ccd6b44bcab02e8425b4d56b79306f92150fbe
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-03-26 00:02:52 +0530

    Updating

--- A_new.py
+++ A_new.py
@@ -1,1 +1,13 @@
-def selection_sort 
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+arr = [2,4,2,2,5,8,8,54,3,8]
+selection_sort(arr)
+print(arr)
