803f3c951087207421fb118fda8257ad923c31f9
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-03-25 23:55:49 +0530

    Updating sorting algos

--- A_new.py
+++ A_new.py
@@ -1,10 +1,1 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n-1):
-        for j in range(n-1-i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-
-arr = [5,3,6,7,4,2,4,79,1]
-bubble_sort(arr)
-print(arr)
+def selection_sort 
