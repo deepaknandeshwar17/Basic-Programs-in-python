ff5b7159432c2d91cc8e4f33a50351c67316dd92
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-05-22 13:08:28 +0530

    Updating with two sum problem

--- A_new.py
+++ A_new.py
@@ -1,13 +1,8 @@
-def selection_sort(arr):
+def insertion_sort(arr):
     n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
 
-    for i in range(n-1):
-        min_index = i
-        for j in range(i+1, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-
-arr = [2,4,2,2,5,8,8,54,3,8]
-selection_sort(arr)
-print(arr)
+        while j >= 0 and arr[j] > key:
+            arr[j+1] =
