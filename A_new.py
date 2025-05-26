e3eabc418b680603fa54314f0822b57c42b63a36
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-05-26 13:06:03 +0530

    Updating kadanes algorithm

--- A_new.py
+++ A_new.py
@@ -1,11 +1,10 @@
-def two_sum_hash(nums,target):
-    hashmap = {}
-    for i, num in enumerate(nums):
-        diff = target - num
-        if diff in hashmap:
-            return [hashmap[diff],i]
-        hashmap[num]=i
-    
-nums = [2,7,6,5]
-target = 8
-print(two_sum_hash(nums,target))
+def max_subarray(nums):
+    current_sum = max_sum = nums[0]
+
+    for num in nums[1:]:
+        current_sum = max(num, current_sum+num)
+        max_sum = max(current_sum,max_sum)
+
+    return max_sum
+
+print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
