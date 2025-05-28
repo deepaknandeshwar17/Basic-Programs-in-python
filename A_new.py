a7d75968355555c77a78d5a3dd24c7d21af7b948
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-05-28 23:26:05 +0530

    Updating

--- A_new.py
+++ A_new.py
@@ -1,10 +1,21 @@
-def max_subarray(nums):
-    current_sum = max_sum = nums[0]
+def max_product(nums):
+    if not nums:
+        return 0
+    
+    max_prod = min_prod = result = nums[0]
 
-    for num in nums[1:]:
-        current_sum = max(num, current_sum+num)
-        max_sum = max(current_sum,max_sum)
+    for i in range(1, len(nums)):
+        num = nums[i]
 
-    return max_sum
+        if num < 0:
+            max_prod, min_prod = min_prod, max_prod
+        
+        max_prod = max(num, num*max_prod)
+        min_prod = min(num, num*min_prod)
 
-print(max_subarray([-2,1,-3,4,-1,2,1,-5,4]))
+        result = max(max_prod, result)
+    
+    return result
+
+nums = [1,2,3,4,-5,6]
+print(max_product(nums))
