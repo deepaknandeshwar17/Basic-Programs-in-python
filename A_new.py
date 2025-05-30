6b66f49cd31b92cb0842925a87333ccd471d059f
deepaknandeshwar17 <deepaknandeshwar17@gmail.com>
2025-05-30 23:47:09 +0530

    Updating sums

--- A_new.py
+++ A_new.py
@@ -41,6 +41,46 @@ def find_min_with_dup(nums):
             right = right - 1
     return left,nums[left]
 
+
+
+def three_sum(nums):
+    nums.sort()
+    n = len(nums)
+    result = []
+
+    for i in range(nums):
+        if i > 0 and nums[i] == nums[i+1]:
+            continue
+       
+        target = -nums[i]
+        left = i+1
+        right = n-1
+
+        while left < right:
+            curr_sum = nums[left] + nums[right]
+
+            if curr_sum == target:
+                result.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left+1]:
+                    left = left + 1
+                while left < right and nums[right] ==  nums[right-1]:
+                    right = right -1 
+                left = left +1
+                right = right -1 
+            elif curr_sum < target:
+                left = left +1 
+            else:
+                right = right -1 
+        return result
+    
+
+
+
+
+
+
+
 # nums = [1,2,3,4,5,6,7]
 nums = [3,4,5,6,7,1,2]
 target = 2
