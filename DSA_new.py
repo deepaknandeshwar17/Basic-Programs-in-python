e833c6003d9c1d1cbe34fe13246d8c86d4334f03
Deepak Nandeshwar <deepaknandeshwar17@gmail.com>
2026-01-12 00:40:33 +0530

    Updating Two pointer

--- DSA_new.py
+++ DSA_new.py
@@ -1,1 +1,23 @@
-print("Describe what to build next!")
+arr = [1,2,3,4,5,6,7]
+
+target = 5
+
+left = 0
+right = len(arr)-1
+
+found = False
+
+while left < right:
+    curr_sum = arr[left] + arr[right]
+
+
+    if curr_sum == target:
+        found = True
+        print(left, right)
+        break
+    elif curr_sum < target:
+        left +=1 
+    else:
+        right-=1
+
+print(found)
