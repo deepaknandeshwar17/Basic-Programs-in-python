4cfe79d50dd6611752e78b712b1d94fb58f0d82d
Deepak Nandeshwar <deepaknandeshwar17@gmail.com>
2026-01-16 09:31:21 +0530

    adding sliding window

--- DSA_new.py
+++ DSA_new.py
@@ -1,23 +1,20 @@
 arr = [1,2,3,4,5,6,7]
-
-target = 5
+target = 12
 
 left = 0
 right = len(arr)-1
 
-found = False
-
 while left < right:
     curr_sum = arr[left] + arr[right]
 
-
     if curr_sum == target:
-        found = True
-        print(left, right)
+        print(f'The sum found at {arr[left]} + {arr[right]} = {curr_sum}' )
         break
     elif curr_sum < target:
-        left +=1 
+        left+=1
     else:
         right-=1
+else:
+    print("Not found")
 
-print(found)
+# print(arr)
